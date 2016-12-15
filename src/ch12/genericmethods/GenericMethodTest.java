745d9bfe8ce9d4d181cb67d658e1494574d1e147
Yaamaidie <lijun_bigface@163.com>
2016-12-15 10:24:12 +0800

    some cases for generic programming in chapter 12

---
+++ src/ch12/genericmethods/GenericMethodTest.java
@@ -0,0 +1,24 @@
+package ch12.genericmethods;
+
+/**
+ * 泛型方法
+ * @author lee
+ *
+ */
+public class GenericMethodTest {
+	
+	public static void main(String[] args) {
+		Double arr = ArrayAlg.getMiddle(new Double[]{11.0, 12.0, 11.5});
+		System.out.println(arr);
+		
+	}
+	
+}
+
+class ArrayAlg {
+	
+	public static <T> T getMiddle(T[] a) {
+		return a[a.length / 2];
+	}
+	
+}
