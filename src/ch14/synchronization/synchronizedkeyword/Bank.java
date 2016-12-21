adebca404302a5215605b6c9fda5757410cd583b
Yaamaidie <lijun_bigface@163.com>
2016-12-21 16:11:41 +0800

    add a case for synchronized keyword in chapter 14

---
+++ src/ch14/synchronization/synchronizedkeyword/Bank.java
@@ -0,0 +1,79 @@
+package ch14.synchronization.synchronizedkeyword;
+
+/**
+ * 有一些账户的银行
+ * 
+ * @author lee
+ *
+ */
+public class Bank {
+
+	private final double[] accounts;
+
+	/**
+	 * 构造器
+	 * 
+	 * @param n
+	 *            账户的数量
+	 * @param initialBalance
+	 *            每个账户的初始余额
+	 */
+	public Bank(int n, double initialBalance) {
+		accounts = new double[n];
+		for (int i = 0; i < accounts.length; i++) {
+			accounts[i] = initialBalance;
+		}
+	}
+
+	/**
+	 * 从一个账户交易到另一个账户
+	 * 
+	 * @param from
+	 *            交易来源
+	 * @param to
+	 *            交易对象
+	 * @param amount
+	 *            交易数额
+	 * @throws InterruptedException
+	 */
+	public synchronized void transfer(int from, int to, double amount)
+			throws InterruptedException {
+
+		while (accounts[from] < amount) {
+			wait();
+		}
+
+		System.out.println(Thread.currentThread());
+		accounts[from] -= amount;
+		System.out.printf(" %10.2f from %d to %d", amount, from, to);
+		accounts[to] += amount;
+		System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+
+		notifyAll();
+
+	}
+
+	/**
+	 * 获取所有账户的余额和
+	 * 
+	 * @return
+	 */
+	public synchronized double getTotalBalance() {
+		double sum = 0;
+		for (double a : accounts) {
+			sum += a;
+		}
+
+		return sum;
+	}
+
+	/**
+	 * 获得该银行的账户状态
+	 * 
+	 * @return 账户数量
+	 */
+	public int size() {
+		return accounts.length;
+	}
+
+}
