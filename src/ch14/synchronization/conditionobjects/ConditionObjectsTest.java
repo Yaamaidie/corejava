2ab777c69d009f48b0cd3ad9f458c9bbfb133e84
Yaamaidie <lijun_bigface@163.com>
2016-12-21 16:10:18 +0800

    add a case for condition objects in chapter 14

---
+++ src/ch14/synchronization/conditionobjects/ConditionObjectsTest.java
@@ -0,0 +1,21 @@
+package ch14.synchronization.conditionobjects;
+
+/**
+ * 演示锁对象
+ * @author lee
+ *
+ */
+public class ConditionObjectsTest {
+	public static final int NACCOUNTS = 100;
+	public static final double INITIAL_BALANCE = 1000;
+	
+	public static void main(String[] args) {
+		Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+		for (int i = 0; i < NACCOUNTS; i++) {
+			TranferRunnable r = new TranferRunnable(b, i, INITIAL_BALANCE);
+			Thread t = new Thread(r);
+			t.start();
+		}
+	}
+	
+}
