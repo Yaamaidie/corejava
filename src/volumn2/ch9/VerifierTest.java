83ca993b1aa97ef1e5866a3bb2733c97d484315b
lijun <lijun_bigface@163.com>
2018-05-30 15:29:49 +0800

    测试校验器

---
+++ src/volumn2/ch9/VerifierTest.java
@@ -0,0 +1,19 @@
+package volumn2.ch9;
+
+/**
+ * Created by HP-PC on 2018/5/28.
+ */
+public class VerifierTest {
+    public static void main(String[] args) {
+        System.out.println("1+2==" + fun());
+    }
+
+    public static int fun() {
+        int m;
+        int n;
+        m = 1;
+        n = 2;
+        int r = m + n;
+        return r;
+    }
+}
